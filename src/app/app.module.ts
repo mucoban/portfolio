be8e4821ecc110a12018c53872c5707e8c84d206
Mücahit Çoban <muco@muco>
2022-10-28 19:52:39 +0300

    basics established

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgScrollbarModule} from "ngx-scrollbar";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgScrollbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
