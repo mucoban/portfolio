bb1816c747891d206c83a72a9e4401f8204a5620
Mücahit Çoban <muco@muco>
2022-11-01 00:01:49 +0300

    auto-scroll functionality, basics finished, wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import {Subject, takeUntil, tap} from "rxjs";
 })
 export class AppComponent implements AfterViewInit, OnDestroy {
 
-  private firstscroll: boolean;
+  activeMitem: number = 1
+  private skipEventListener: boolean;
   private oldScrollLinePosition: number = 0
   private destroy$ = new Subject<boolean>()
   @ViewChild(NgScrollbar, { static: true }) scrollbarRef: NgScrollbar;
@@ -22,35 +23,69 @@ export class AppComponent implements AfterViewInit, OnDestroy {
       .pipe(
         takeUntil(this.destroy$),
         tap((e: any) => this.ngZone.run(() => {
-        if (this.firstscroll) return
-        this.firstscroll = true
-         const heightLimit = window.innerHeight - 260
-          const parentScrollTopLine = e.target.scrollTop + heightLimit / 2
-          console.log('tap', parentScrollTopLine)
-          this.sections.map((section, index) => {
-             const sectionOffsetTop = section.nativeElement.offsetTop
-             if (section.nativeElement.scrollHeight === heightLimit) {
-               if (parentScrollTopLine >= sectionOffsetTop
-                && parentScrollTopLine <= sectionOffsetTop + heightLimit
-               ) {
-                 const nextSectionIndex = index + (this.oldScrollLinePosition <= parentScrollTopLine ? 1 : -1)
-                 const nextSection = this.sections.find((s, index) => index === nextSectionIndex )
-                 console.log(nextSection.nativeElement.offsetTop)
-                 if (nextSection) { this.scrollbarRef.scrollTo({ top: nextSection.nativeElement.offsetTop, duration: 800 }) }
+            if (this.skipEventListener) {
+              return
+            }
+            console.log('scroll')
+            const heightLimit = window.innerHeight - 277
+            const parentScrollTopLine = e.target.scrollTop + heightLimit / 2
+            this.sections.map((section, index) => {
+               const sectionOffsetTop = section.nativeElement.offsetTop
+               if (1 || section.nativeElement.scrollHeight === heightLimit) {
+                 const sectionBottom = sectionOffsetTop + section.nativeElement.scrollHeight
+                 const sectionBottomUpper = sectionBottom - heightLimit / 2
+                 let addToIndex = 0
+                 if (parentScrollTopLine >= sectionBottomUpper
+                  && parentScrollTopLine <= sectionBottom
+                 ) {
+                    addToIndex = 1
+                 }
+                 else if (parentScrollTopLine >= sectionOffsetTop
+                  && parentScrollTopLine <= sectionOffsetTop + heightLimit / 2
+                 ) {
+                    addToIndex = -1
+                 }
+
+                 if (addToIndex) {
+                   const nextSectionIndex = index + addToIndex
+                   const nextSection = this.sections.find((s, index) => index === nextSectionIndex )
+                   if (nextSection) {
+                     this.activeMitem = nextSectionIndex + 1
+                     this.scroll({ top: nextSection.nativeElement.offsetTop, duration: 600 })
+                   }
+                 }
                }
-             }
-          })
-          this.oldScrollLinePosition = parentScrollTopLine
-        }))
+            })
+
+          }),
+        ),
+        tap((e) => {
+            this.oldScrollLinePosition = e.target.scrollTop
+        })
       )
       .subscribe();
-
-
   }
 
   ngOnDestroy() {
     this.destroy$.next(true)
     this.destroy$.unsubscribe()
+  }
+
+  onClickMitem(sectionIndex: number) {
+    const nextSection = this.sections.find((s, index) => index === sectionIndex)
+    if (nextSection) {
+      console.log(nextSection.nativeElement.offsetTop)
+      this.activeMitem = sectionIndex + 1
+      this.scroll({ top: nextSection.nativeElement.offsetTop, duration: 600 })
+    }
+  }
+
+  private scroll(options: any) {
+    this.skipEventListener = true;
+    this.scrollbarRef.scrollTo(options)
+    setTimeout(() => { this.skipEventListener = false
+      console.log('falsed')
+    }, options.duration + 600)
   }
 
 }
