0160de4e26127e2089f127017f442f9290ee309b
mucoban <webdeveloper.mucahitcoban@gmail.com>
2023-08-02 01:29:28 +0300

    feature&refactor&style: add side buttons, refactor code, edit style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,13 +17,34 @@ export class AppComponent {
 
   constructor() {}
 
-  onClickMitem(sectionIndex: number) {
+  goToPrevNextSection(isNext?: boolean) {
+    const index = !isNext ? this.activeMitem - 1 - 1 : this.activeMitem - 1 + 1
+    if (index < 0 && index >= this.sections.length) return
+    this.onClickMitem( index)
+  }
+
+  onClickMitem(sectionIndex: number, noScroll?: boolean) {
     const nextSection = this.sections.find((s, index) => index === sectionIndex)
     if (nextSection) {
       this.activeMitem = sectionIndex + 1
-      const scrollTop = this.sections.toArray()[sectionIndex].nativeElement.offsetTop
-      this.columnPage.nativeElement.scrollTo({ top: scrollTop, behavior: 'smooth' })
+      if (!noScroll) {
+        const scrollTop = this.sections.toArray()[sectionIndex].nativeElement.offsetTop
+        this.columnPage.nativeElement.scrollTo({ top: scrollTop, behavior: 'smooth' })
+      }
     }
+  }
+
+  onScrollColumnPage(event: any) {
+    const targetHeightD2 = event.target.offsetHeight / 2
+    const scrollTop = event.target.scrollTop + targetHeightD2
+
+    this.sections.map((section, index: number) => {
+      const offsetBottom = section.nativeElement.offsetTop + section.nativeElement.offsetHeight
+      if (scrollTop > section.nativeElement.offsetTop && scrollTop < offsetBottom) {
+          this.onClickMitem(index, true)
+      }
+    })
+
   }
 
 }
